af532e7aa1e15c79ef637aa2408d703f2164a5d4
Muzafar-Ali <84478313+Muzafar-Ali@users.noreply.github.com>
2021-07-31 03:43:28 +0500

    Update Assignment1.sol

--- Assignment1.sol
+++ Assignment1.sol
@@ -24,7 +24,6 @@ contract Vehicle {
 }
 
 // Requirement 2) Next Create following Child contracts for a different type of vehicles, that inherits Vehicle Cars, Truck, MotorCycle
-
 contract Cars is Vehicle {
     function service() public pure virtual override returns (string memory) {
         string memory message = "car is being serviced";
@@ -47,7 +46,6 @@ contract Motorcycle is Vehicle {
 }
 
 //Requirement 3) Then create smart contracts for “Alto Mehran, “ Hino, Yamaha, that may inherit the relevant smart contract(s) as in step 2.
-
 contract AltoMehran is Cars {
     function service() public pure override returns (string memory) {
         string memory message = "Alto Mehran car is being serviced";
